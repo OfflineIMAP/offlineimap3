58596767ef098521fad2b57dba43063a7a5520df
Rodolfo García Peñas (kix) <kix@kix.es>
2020-08-29 20:04:40 +0200

    Reformat offlineimap/__init__.py
    Add some spaces, remove lines,... now format is better (lintian).

--- offlineimap/__init__.py
+++ offlineimap/__init__.py
@@ -2,12 +2,12 @@ __all__ = ['OfflineImap']
 
 __productname__ = 'OfflineIMAP'
 # Expecting trailing "-rcN" or "" for stable releases.
-__version__     = "7.3.0"
-__copyright__   = "Copyright 2002-2019 John Goerzen & contributors"
-__author__      = "John Goerzen"
-__author_email__= "offlineimap-project@lists.alioth.debian.org"
+__version__ = "7.3.0"
+__copyright__ = "Copyright 2002-2019 John Goerzen & contributors"
+__author__ = "John Goerzen"
+__author_email__ = "offlineimap-project@lists.alioth.debian.org"
 __description__ = "Disconnected Universal IMAP Mail Synchronization/Reader Support"
-__license__  = "Licensed under the GNU GPL v2 or any later version (with an OpenSSL exception)"
+__license__ = "Licensed under the GNU GPL v2 or any later version (with an OpenSSL exception)"
 __bigcopyright__ = """%(__productname__)s %(__version__)s
   %(__license__)s""" % locals()
 __homepage__ = "http://www.offlineimap.org"
