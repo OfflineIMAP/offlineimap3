3cdc99946ec075ecf97191e5ef01c396dff6b60c
Keith Bowes <keithbowes@users.noreply.github.com>
2024-08-18 16:38:49 -0400

    Might as well replace the obsolete distutils (removed from Python 3.12) package with setuptools too
    Signed-off-by: Keith Bowes <keithbowes@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
-from distutils.core import setup, Command
+from setuptools import setup, Command
 import offlineimap
 import logging
 from test.OLItest import TextTestRunner, TestLoader, OLITestLib
